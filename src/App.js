bedb068f15f15446230578e603b0d8a332adbcf5
Agustin Balart <balartagustin@gmail.com>
2022-09-11 20:54:57 -0300

    Agregando boton con estado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/Nav/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 const  App = () => {
   return (
   <>
     <NavBar />
     <ItemListContainer greeting={"Aqui vamos a agregar el catálogo"}/>
+    <ItemCount/>
   </> 
   );
 }
