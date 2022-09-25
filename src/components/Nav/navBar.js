3f7d843037ce547fdf9992f70cefa16f9e47f3a3
Agustin Balart <balartagustin@gmail.com>
2022-09-25 20:17:26 -0300

    Agrego HOMO en imagen

--- src/components/Nav/navBar.js
+++ src/components/Nav/navBar.js
@@ -4,8 +4,7 @@ import './NavBar.css'
 import logo from '../../imagenes/Logo.JPG'
 import 'bootstrap/dist/css/bootstrap.css';
 import CartWidget from '../../components/CartWidget/CartWidget'
-
-import {Lik,NavLink} from 'react-router-dom'
+import {Link,NavLink} from 'react-router-dom'
 
 
 const categories = [
@@ -15,17 +14,22 @@ const categories = [
   {id:3,title: 'jewelery', route: '/categoria/jewelery'}
 ]
 
- const Nav = ({categorias}) => {  //Declaro un elemento JSX, sera hijo de App o del componente que quiera
+ const NavBar = ({categorias}) => {  //Declaro un elemento JSX, sera hijo de App o del componente que quiera
     
   return (
       <>
+      <header>
+      <Link to={'/'}>
+        <img className="brand-logo center imagenLogo rounded mx-auto d-block" src={logo} ></img>
+
+      </Link>
         <nav  className="navbar-light bg-light estiloLinkNavegacion">
             {categories.map((category) => {
                 return <NavLink key={category.id} to={category.route} style={styles.links}>{category.title}</NavLink>
             })}
         </nav>
-             <img className="brand-logo center imagenLogo rounded mx-auto d-block" src={logo} ></img>
             <CartWidget/>
+      </header>
       </>     
           )
          
@@ -37,4 +41,4 @@ const categories = [
     }
 }
  
- export default Nav //Necesario
+ export default NavBar //Necesario
