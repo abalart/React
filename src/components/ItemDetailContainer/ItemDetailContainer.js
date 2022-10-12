045fabb367aa356d439f7e1e015e74507aba5a34
Agustin Balart <balartagustin@gmail.com>
2022-10-11 21:18:13 -0300

    Invocando a firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { CircularProgress } from "@mui/material";
 import ItemDetail  from "../ItemDetail/ItemDetail"
 import { useParams } from "react-router-dom";
+import { db } from "../../firebase/firebase";
 import { doc, getDoc, collection } from "firebase/firestore";
 
 const ItemDetailContainer = () => {  //Este componente le pasa a ItemDetails la info de productos por medio del estado SetProducto,
@@ -14,21 +15,22 @@ const ItemDetailContainer = () => {  //Este componente le pasa a ItemDetails la 
   const [error, setError] = useState(false);
 
 
-  useEffect(() => {
-    const url = `${'https://fakestoreapi.com/products/'}${id}`;
-    const getItem = async () => {
-      try {
-        const resp = await fetch(url);
-        const data = await resp.json();
-        setProduct({...data, stock: Math.floor(Math.random() * 20)});
-      } catch (error) {
-        console.error(error);
+useEffect(() => {
+    const productCollection = collection(db, "products");
+    const refDoc = doc(productCollection, id);
+    getDoc(refDoc)
+      .then((result) => {
+        setProduct({
+          id: result.id,
+          ...result.data(),
+        });
+      })
+      .catch(() => {
         setError(true);
-      } finally {
+      })
+      .finally(() => {
         setLoading(false);
-      }
-    };
-    getItem();
+      });
   }, [id]);
 
 
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';   
-import {products} from '../../Assets/productos' //Traigo los productos a mostrar desde este archivo
+//import {products} from '../../Assets/productos' //Traigo los productos a mostrar desde este archivo
 import {useEffect,useState} from 'react'
 import ItemList from '../ItemList/ItemList';
 import { CircularProgress } from '@mui/material';
 import { useParams } from 'react-router-dom';
+import { db } from "../../firebase/firebase";
+import { getDocs, collection, query, where } from "firebase/firestore";
 
 
 //Este componente va a listar los items
@@ -21,26 +23,30 @@ const [error, setError] = useState(false);
 
  
 
-const URL_CATEGORY = 'https://fakestoreapi.com/products/category/'
-const BASE = 'https://fakestoreapi.com/products'
+//const URL_CATEGORY = 'https://fakestoreapi.com/products/category/'
+//const BASE = 'https://fakestoreapi.com/products'
 
 
 useEffect(() => {
-    const url = id ? `${URL_CATEGORY}${id}` : BASE;
-    console.log("URL "+url);
-    const getItems = async () => {
-      try {
-        const respuesta = await fetch(url);
-        const data = await respuesta.json();
-        setProducts(data);
-      } catch (err) {
-        console.error(err);
+    const productsCollection = collection(db, "products");
+    const q = query(productsCollection, where("category", "==", "jewelery"));
+
+    getDocs(productsCollection)
+      .then((data) => {
+        const lista = data.docs.map((product) => {
+          return {
+            ...product.data(),
+            id: product.id,
+          };
+        });
+        setProducts(lista);
+      })
+      .catch(() => {
         setError(true);
-      } finally {
+      })
+      .finally(() => {
         setLoading(false);
-      }
-    };
-    getItems();
+      });
   }, [id]);
 
 
